59c90e5256a0329fe43381d552e296a264e55a27
Jamie Pham <jtkp66@gmail.com>
2018-10-24 11:45:48 -0700

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+
 
 const db = require('./config/keys').mongoURI
 
@@ -16,6 +19,14 @@ mongoose
     .catch(err => console.log(err));
 
     app.use('/api/items', items);
+
+    if(process.env.NODE_ENV === 'production') {
+        app.use(express.static('client/build'));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
+    }
 
     const port = process.env.PORT || 5000;
 
